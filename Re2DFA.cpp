d0384d60cf2320dfd4affe279981ce8010b84bd8
LetMeFly666 <814114971@qq.com>
2022-03-29 17:15:54 +0800

    Finished regular to reverse polish

--- Re2DFA.cpp
+++ Re2DFA.cpp
@@ -1,5 +1,11 @@
 #include "Re2DFA.h"
 
+/**错误码errorCode
+* 0   无误
+* 1   )找不到(
+*/
+int errorCode = 0;
+
 Re2DFA::Re2DFA(QWidget *parent) : QWidget(parent) {
     // 主窗口
     ui.setupUi(this);
@@ -17,9 +23,10 @@ Re2DFA::Re2DFA(QWidget *parent) : QWidget(parent) {
 
 void Re2DFA::on_pushButton_clicked() {
     QString reFormatted = addConOp2Re(ui.lineEdit->text());
-    ui.label_ReformatReString->setText(reFormatted);
+    ui.label_ReformatReString->setText(showString(reFormatted));
+    // TODO: EOORO的处理
     QString reversePolish = Re2RePo(reFormatted);
-    ui.label_ReversePolish->setText(reversePolish);
+    ui.label_ReversePolish->setText(showString(reversePolish));
 }
 
 void Re2DFA::on_pushButton_Connect_clicked() {
@@ -47,16 +54,112 @@ void Re2DFA::on_pushButton_Rightbracket_clicked() {
     ui.lineEdit->setText(ui.lineEdit->text() + ")");
 }
 
+/* 将用,.代替的字符串转换为ε·代表的字符串 */
+const QString showString(QString origin) {
+    string s = origin.toStdString();
+    string ans;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == ',')
+            ans += "ε";
+        else if (s[i] == '.')
+            ans += "·";
+        else
+            ans += s[i];
+    }
+    return QString::fromStdString(ans);
+}
+
 /* 添加“·”到正则表达式 */
 QString addConOp2Re(QString re) {
     string s = re.toStdString();
+    string temp;
+    for (int i = 0; i + 1 < s.size(); i++) {
+        if ((unsigned char)s[i] == 194 && (unsigned char)s[i + 1] == 183) {  // ·
+            temp += '.';
+            i++;
+        }
+        else if ((unsigned char)s[i] == 206 && (unsigned char)s[i + 1] == 181) {  // ε
+            temp += ',';
+            i++;
+        }
+        else {
+            temp += s[i];
+        }
+    }
+    temp += s[s.size() - 1];
+    s = temp;
     string ans;
-    ans = "55";
+    for (int i = 0; i + 1 < s.size(); i++) {
+        ans += s[i];
+        if ((Char.count(s[i]) && Char.count(s[i + 1])) || (Char.count(s[i]) && s[i + 1] == '(') || (s[i] == ')' && Char.count(s[i + 1]))) {
+            ans += '.';
+        }
+    }
+    ans += s[s.size() - 1];
     return QString::fromStdString(ans);
 }
 
 QString Re2RePo(QString re) {
+    string s = re.toStdString();
     string ans;
-
+    stack<char> op;
+    /* 比较a和b的优先级(不包含括号) a>b返回正数，a<b返回负数，a=b返回0 */
+    auto priority = [](char a, char b) {
+        // * > · > |
+        auto c2i = [](char c) {
+            if (c == '*') {
+                return 2;
+            }
+            else if (c == '.') {
+                return 1;
+            }
+            else if (c == '|') {
+                return 0;
+            }
+            else {
+                return -1;
+            }
+        };
+        return c2i(a) - c2i(b);
+    };
+    for (int i = 0; i < s.size(); i++) {
+        if (Char.count(s[i])) {
+            ans += s[i];
+            continue;
+        }
+        if (s[i] == '(') {
+            op.push(s[i]);
+        }
+        else if (s[i] == ')') {
+            while (true) {
+                if (op.empty()) {
+                    errorCode = 1;
+                    return "Error1: )without(";
+                }
+                if (op.top() == '(') {
+                    op.pop();
+                    break;
+                }
+                ans += op.top();
+                op.pop();
+            }
+        }
+        else {
+            while (true) {
+                if (op.empty() || op.top() == '(' || priority(op.top(), s[i]) < 0) {
+                    op.push(s[i]);
+                    break;
+                }
+                else {
+                    ans += op.top();
+                    op.pop();
+                }
+            }
+        }
+    }
+    while (op.size()) {
+        ans += op.top();
+        op.pop();
+    }
     return QString::fromStdString(ans);
 }
--- Re2DFA.h
+++ Re2DFA.h
@@ -3,6 +3,7 @@
 #include <QtWidgets/QWidget>
 #include "ui_Re2DFA.h"
 #include <stack>
+#include <set>
 using namespace std;
 
 class Re2DFA : public QWidget {
@@ -30,5 +31,13 @@ private:
     Ui::Re2DFAClass ui;
 };
 
+const QString showString(QString origin);
 QString addConOp2Re(QString re);
 QString Re2RePo(QString re);
+
+static set<char> Char = {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    ','  // ¦Å
+};
