f701a5f8d4a3bb0c9b500787b54f7804d9f07e3d
LetMeFly666 <814114971@qq.com>
2022-03-29 12:03:11 +0800

    UI设计初步

--- Re2DFA.cpp
+++ Re2DFA.cpp
@@ -1,5 +1,35 @@
 #include "Re2DFA.h"
 
 Re2DFA::Re2DFA(QWidget *parent) : QWidget(parent) {
+    // 主窗口
     ui.setupUi(this);
+    setWindowTitle("Regular To DFA - LetMeFly");
+    setFixedSize(1310, 865);
+
+    // 便捷输入
+    ui.pushButton_Connect->setToolTip("连接符（中文的“`”）");
+}
+
+void Re2DFA::on_pushButton_clicked() {
+    
+}
+
+void Re2DFA::on_pushButton_Connect_clicked() {
+    ui.lineEdit->setText(ui.lineEdit->text() + "·");
+}
+
+void Re2DFA::on_pushButton_Empty_clicked() {
+
+}
+
+void Re2DFA::on_pushButton_Or_clicked() {
+
+}
+
+void Re2DFA::on_pushButton_Leftbracket_clicked() {
+
+}
+
+void Re2DFA::on_pushButton_Rightbracket_clicked() {
+
 }
--- Re2DFA.h
+++ Re2DFA.h
@@ -9,6 +9,19 @@ class Re2DFA : public QWidget {
 public:
     Re2DFA(QWidget *parent = Q_NULLPTR);
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_Connect_clicked();
+
+    void on_pushButton_Empty_clicked();
+
+    void on_pushButton_Or_clicked();
+
+    void on_pushButton_Leftbracket_clicked();
+
+    void on_pushButton_Rightbracket_clicked();
+
 private:
     Ui::Re2DFAClass ui;
 };
